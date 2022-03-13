0039193bced12a34e67b67632d7a7ab1930c234d
manasesortez <manasesortez1234@hotmail.com>
2022-03-12 19:05:21 -0600

    uso de notificaciones

---
+++ app/src/main/java/com/amto_dev/uso_notificaciones/MainActivity.java
@@ -0,0 +1,13 @@
+package com.amto_dev.uso_notificaciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
