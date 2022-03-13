ce198bea4489448e65e42717fac8ca112546778c
manasesortez <manasesortez1234@hotmail.com>
2022-03-13 14:54:25 -0600

    uso de notificaciones

--- app/src/main/java/com/amto_dev/uso_notificaciones/MainActivity.java
+++ app/src/main/java/com/amto_dev/uso_notificaciones/MainActivity.java
@@ -2,12 +2,25 @@ package com.amto_dev.uso_notificaciones;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.onesignal.OneSignal;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String ONESIGNAL_APP_ID = "be5dfb6c-d3bb-4ca3-8b3b-84a86138a9c2";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+
+
     }
 }
